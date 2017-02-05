c0edd45483b31ae6f5b535c99f6920d58313e774
gregdegruy <gregory.degruy@gmail.com>
2017-02-05 11:26:48 -0800

    +- replaced admin key with query key

--- app/services/searchService.js
+++ app/services/searchService.js
@@ -1,16 +1,20 @@
-var searchEndpoint = `http://botcommerce.search.windows.net/indexes/temp/docs?{$queryString}&api-version=2016-09-01`
-// shopping, view cart and check out
+// not the actual [secondary] admin/api-key
+var queryKey = 'D313DF388316446D8E255E783BA327D4'
+let queryString = ''
+var searchEndpoint = `https://botcommercev2.search.windows.net
+/indexes/product/docs?${queryString}&api-version=2016-09-01&api-key=${queryKey}`
+
 module.exports = {
 
-    search: function search(term) {
+    search: function search (term) {
         // search=helmet&facet=Color AND $filter=Color eq ‘Red’
-        var queryString = 'search=helmet&facet=Color'
+        queryString = 'search=helmet&facet=Color'
 
         require('http').get(searchEndpoint, (res) => {
-            res.setEncoding('utf8');
+            res.setEncoding('utf8')
             res.on('data', function (body) {
-                console.log(body);
-            });
-        });
+                console.log(body)
+            })
+        })
     }
 }
