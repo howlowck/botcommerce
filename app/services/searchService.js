ed2ad30a4058acc1c0ca2ffd708daeb588021dc9
gregdegruy <gregory.degruy@gmail.com>
2017-02-05 09:42:58 -0800

    + search function

--- app/services/searchService.js
+++ app/services/searchService.js
@@ -1,3 +1,8 @@
 module.exports = function searchService() {
 
+    var searchEndpoint = "https://botcommerce.search.windows.net/indexes/temp/docs?{$queryString}&api-version=2016-09-01";
+
+    function search(term) {
+        var queryString = "";
+    }
 }
