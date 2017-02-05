01820f8518776ed6d04ec8728490a75f62a2c24e
Justine Cocchi <jucocchi@microsoft.com>
2017-02-05 13:59:08 -0800

    working on product search dialog

--- app/productSearch.js
+++ app/productSearch.js
@@ -1,7 +1,49 @@
+const builder = require('botbuilder')
+
+const extractEntities = (session, args) => {
+  var foundEntities = []
+
+  // Try to find entities
+  var product = builder.EntityRecognizer.findEntity(args.entities, 'productName')
+  var color = builder.EntityRecognizer.findEntity(args.entities, 'color')
+
+  if (product) {
+    // We found a product entity
+    foundEntities.push(product)
+
+    if (color) {
+      // We found a color entity
+      foundEntities.push(color)
+    } else {
+      // If no color was found, send null
+      foundEntities.push(null)
+    }
+
+    return foundEntities
+  }
+
+  // We didn't find a product entity
+  return undefined
+}
+
 module.exports = function (bot) {
   bot.dialog('/productSearch', [
     function (session, args, next) {
-      session.endDialog('PRODUCT DIALOG! YAY')
+      var entities = extractEntities(session, args)
+
+      if (entities) {
+        // Call Azure Search API
+        // Display cards
+        session.send('You want to ')
+      } else {
+        builder.Prompts.text('What product would you like to search for?')
+      }
+    },
+    function (session, args, next) {
+      var product = args.response
+
+      session.send('You want to search for ' + product)
+      session.endDialog()
     }
   ])
 }
