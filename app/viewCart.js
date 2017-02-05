800ceb21849b0a846f23a3f28f354bd2636ab72c
Kevin Leung <leung.kevin23@gmail.com>
2017-02-05 13:33:18 -0800

    SQL queries

--- app/viewCart.js
+++ app/viewCart.js
@@ -4,18 +4,17 @@ const Request = require('tedious').Request
 module.exports = function (bot, db) {
   bot.dialog('/viewCart', [
     (session) => {
-      builder.Prompts.text(session, 'Hi! What is your name?')
-    },
-    (session, results) => {
-      const firstName = results.response
-      const nameRequest = new Request(`SELECT * FROM dbo.Customers as c WHERE c.firstName = '${firstName}';`, function (err, rowCount) {
+      // instantiate cart as empty array
+      session.userData.cart = []
+      // find user by skypeId
+      const skypeId = session.message.address.user
+      const nameRequest = new Request(`SELECT * FROM dbo.Customers as c WHERE c.skypeId = '${skypeId}';`, function (err) {
         if (err) {
           console.log(err)
-        } else {
-          console.log(rowCount + ' rows')
-        }
+        } 
       })
 
+      // search each column in row for name info
       nameRequest.on('row', function (columns) {
         let firstName = ''
         let lastName = ''
@@ -29,18 +28,55 @@ module.exports = function (bot, db) {
           }
         })
 
-        session.send(`Hi ${firstName} ${lastName}`)
+        // store user's name in userData
         session.userData.name = firstName + ' ' + lastName
-      
-        session.endDialog()
       })
 
       db.execSql(nameRequest)
       // session.userData.name = results.response
       // session.endDialog()
-    },
-    (session, args, next) => {
-      session.endDialog('VIEWCART DIALOG! YAY')
     }
   ])
 }
+
+    // (session) => {
+    //   builder.Prompts.text(session, 'Hi! What is your name?')
+    // },
+    // (session, results) => {
+    //   const firstName = results.response
+    //   const nameRequest = new Request(`SELECT * FROM dbo.Customers as c WHERE c.firstName = '${firstName}';`, function (err, rowCount) {
+    //     if (err) {
+    //       console.log(err)
+    //     } else {
+    //       console.log(rowCount + ' rows')
+    //     }
+    //   })
+
+    //   nameRequest.on('row', function (columns) {
+    //     let firstName = ''
+    //     let lastName = ''
+
+    //     columns.forEach((column) => {
+    //       if (column.metadata.colName === 'firstName') {
+    //         firstName = column.value
+    //       }
+    //       if (column.metadata.colName === 'lastName') {
+    //         lastName = column.value
+    //       }
+    //     })
+
+    //     session.send(`Hi ${firstName} ${lastName}`)
+    //     session.userData.name = firstName + ' ' + lastName
+      
+    //     session.endDialog()
+    //   })
+
+    //   db.execSql(nameRequest)
+    //   // session.userData.name = results.response
+    //   // session.endDialog()
+    // },
+    // (session, args, next) => {
+    //   session.endDialog('VIEWCART DIALOG! YAY')
+    // }
+//   ])
+// }
