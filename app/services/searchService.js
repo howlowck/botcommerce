54c4c3d7c6b887d7d7e47b13db0c849ef693ca3f
gregdegruy <gregory.degruy@gmail.com>
2017-02-05 11:34:38 -0800

    +- hard coded structure for search query

--- app/services/searchService.js
+++ app/services/searchService.js
@@ -1,8 +1,7 @@
 // not the actual [secondary] admin/api-key
 var queryKey = 'D313DF388316446D8E255E783BA327D4'
-let queryString = ''
-var searchEndpoint = `https://botcommercev2.search.windows.net
-/indexes/product/docs?${queryString}&api-version=2016-09-01&api-key=${queryKey}`
+var queryString = ''
+var searchEndpoint = `https://botcommercev2.search.windows.net/indexes/product/docs?${queryString}&api-version=2016-09-01&api-key=${queryKey}`
 
 module.exports = {
 
@@ -10,7 +9,7 @@ module.exports = {
         // search=helmet&facet=Color AND $filter=Color eq ‘Red’
         queryString = 'search=helmet&facet=Color'
 
-        require('http').get(searchEndpoint, (res) => {
+        require('https').get('https://botcommercev2.search.windows.net/indexes/product/docs?api-version=2016-09-01&api-key=D313DF388316446D8E255E783BA327D4', (res) => {
             res.setEncoding('utf8')
             res.on('data', function (body) {
                 console.log(body)
